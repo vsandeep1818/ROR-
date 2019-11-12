0ace41593a138b2290e85b2fc0388a5c6041a787
Hackodezo <athul@hackodezo.com>
2019-11-12 10:52:05 +0530

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,4 @@ class User < ApplicationRecord
   def user_name_length
     errors.add(:username, "username should b e less than 10 characters") unless self.username.length < 10
   end
-
-
-  "create 4 validations 1.check email regex, 2.password should contain atleast 1 letter 1 digit and one special character, 3.
-   username should be unique 4. password length should be less than 16 "
 end
